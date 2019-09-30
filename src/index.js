b7c70214003468e160732320f37ad4baac6e24d9
Charlie Thanaphansin <achanont@gmail.com>
2019-09-30 11:57:38 -0500

    cannot load marker picture

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-console.log('Hello Trip');
+const mapboxgl = require('mapbox-gl');
+
+mapboxgl.accessToken =
+  'pk.eyJ1IjoiYWNoYW5vbnQiLCJhIjoiY2sxNm04Mzl5MTc0cDNubnp6bTN4OXhmdCJ9.dwEXUIpmeZT4nOfSthZbmA';
+
+const map = new mapboxgl.Map({
+  container: 'map',
+  center: [-87.6354, 41.8885], // FullStack NY coordinates; alternatively, use [-87.6354, 41.8885] for Chicago
+  zoom: 12, // starting zoom
+  style: 'mapbox://styles/mapbox/streets-v10' // mapbox has lots of different map styles available.
+});
+
+const markerDomEl = document.createElement('div');
+markerDomEl.style.width = '32px';
+markerDomEl.style.height = '39px';
+markerDomEl.style.backgroundImage = 'url(http://i.imgur.com/D9574Cu.png)';
+
+new mapboxgl.Marker(markerDomEl).setLngLat([-87.641, 41.895]).addTo(map);
